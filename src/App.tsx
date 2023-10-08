8519feb5700e6b39d3517ba6a801e3882ea75a5d
Arcane Coder <avamit0820@gmail.com>
2023-10-08 17:03:47 +0530

    notes app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { NoteObject } from './models/note';
+
+// components
+import Header from "./components/Header";
+import CreateNote from "./components/CreateNote";
+import Notes from "./components/Notes";
+
+import { Box } from "@mui/material";
 
 function App() {
+
+  const [notes, setNotes] = useState<NoteObject[]>([]);
+
+  useEffect(() => {
+    if (sessionStorage.getItem('notes')) {
+      setNotes(JSON.parse(sessionStorage.getItem('notes') as string));
+    }
+  }, [])
+
+
+  const addNote = (note: NoteObject) => {
+    setNotes([ note, ...notes ]); //change the position of note and notes to print latest notes added in front or back 
+    console.log(sessionStorage);
+    sessionStorage.setItem('notes', JSON.stringify([ note, ...notes ]));
+  }
+
+  const deleteNote = (id: number) => {
+    const updatedNotes = notes.filter(note => note.id !== id);
+    setNotes(updatedNotes); 
+    sessionStorage.setItem('notes', JSON.stringify(updatedNotes));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Box style={{ padding:20 }}>
+        <CreateNote addNote={addNote} />
+        <Notes notes={notes} deleteNote={deleteNote} />
+      </Box>
+    </>
   );
 }
 
---
+++ src/constants/constant.js
@@ -0,0 +1,4 @@
+export const logo = 'favicon.png';
+
+export const TITLE_LIMIT = 30;
+export const TEXT_LIMIT = 150;
